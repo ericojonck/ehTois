4ed3a9372178d6792d0930d295dde01d03d9ac31
NevesFelipe <42079601+NevesFelipe@users.noreply.github.com>
2018-08-03 21:38:23 -0300

    Update README.java

--- README.java
+++ README.java
@@ -30,3 +30,38 @@ public class Main{
     }
     
 }
+class Operacao {
+    public double soma () {
+        return 0;
+    }
+    
+    public double calculaMedia(double n1, double n2) {
+        return (n1 + n2) / 2;
+    }
+    
+    public int multiplicacao () { 
+        return 1;
+    }
+    
+    public double divisao (double n1, double n2) {
+        return n1/n2;
+    }
+    
+    public double subtracao () {
+        return 0; 
+    }
+    
+    public double mediaPonderada (double n1, double n2) {
+        return (n1 + n2)/ 1 + 2;
+    }
+    
+    public double fatorial (double fat) {                                                                                           
+        double f = 0;
+        while (fat > 1){  
+             f *=(fat-1); 
+             fat--; 
+        }
+        return f;
+    }
+       
+}
